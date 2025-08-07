9d84109d9fae0a6612e7fe07c170c7a8ffe35478
OkuhleSotyanya <sotyantyao@gmail.com>
2025-08-07 15:41:46 +0200

    fresh start

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+ 
+  <h1>Welcome to ProConnect</h1>
+  <p>This is the home view of the application.</p>
+  <!-- You can add more content here -->
+  <router-link to="/about">Go to About</router-link>
+  <router-link to="/contact">Go to Contact</router-link>
+  <router-link to="/services">Go to Services</router-link>
+  <router-link to="/profile">Profile</router-link>
+  <router-link to="/dashboard">Dashboard</router-link>
+  <router-link to="/contact-us">Contact Us</router-link>
+  <router-link to="/feedback">Feedback</router-link>
+  <router-link to="/logout">Logout</router-link>
+</template>
+
+<script>
+
+</script>
+
